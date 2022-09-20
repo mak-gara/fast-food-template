092801074c90920c81e93627c8133c2eac520c10
Maksim Garagulia <maksimgaragulia@gmail.com>
2022-09-20 11:38:52 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    loop: true,
+});
+
+new Swiper('.recomend-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 4,
+    loop: true,
+    centeredSlides: true,
+});
