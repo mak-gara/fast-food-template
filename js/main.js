605c5923fee54d46478388fe310df7cde037e7e5
Maksim Garagulia <maksimgaragulia@gmail.com>
2022-09-30 10:39:03 +0300

    added Menu Page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-new Swiper('.image-slider', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true
-    },
-    loop: true,
-});
+window.onload = function() {
+    new Swiper('.image-slider', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true
+        },
+        loop: true,
+    });
+    
+    new Swiper('.recomend-slider', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        slidesPerView: 4,
+        loop: true,
+        centeredSlides: true,
+    });
+};
 
-new Swiper('.recomend-slider', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    slidesPerView: 4,
-    loop: true,
-    centeredSlides: true,
-});
